fdcf256c22c15723207e57bbfaeae5b176d944f7
tyrafero <ashutoshsinghkhadka@gmail.com>
2023-12-31 12:40:19 +1100

    chore: comments edited

--- code/Q1_T2_count_words.py
+++ code/Q1_T2_count_words.py
@@ -2,10 +2,10 @@ import csv
 from collections import Counter
 
 def count_words_in_text(text_file, csv_file):
-    print("I am here")
+    print("Initializing .. \n")
 
     with open(text_file, 'r') as file:
-        print("I am here")
+        print("Reading Text File ..")
 
         text = file.read()
     words = text.split()
@@ -13,11 +13,11 @@ def count_words_in_text(text_file, csv_file):
     top_30_words = word_counts.most_common(30)
 
     with open(csv_file, 'w', newline='') as file:
-        print("I am here")
+        print("Analyzing words ..")
         writer = csv.writer(file)
         writer.writerow(['Word', 'Count'])
         writer.writerows(top_30_words)
-        
+
 text_file = 'output/output.txt'
 csv_file = 'output/word_counts.csv'
 
